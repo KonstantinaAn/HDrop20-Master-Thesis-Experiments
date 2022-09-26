3879a1f857f1ea27585c0b8ba5f15fca7399629f
konnaan <konstantina_anastasiadou@windowslive.com>
2022-09-26 23:43:47 +0300

    k=0.1 for guru

--- experiments/First_Generation/run_experiment_01.py
+++ experiments/First_Generation/run_experiment_01.py
@@ -1,6 +1,6 @@
 from experiments.First_Generation.experiment import Experiment
 
 first_experiment = Experiment('Official_Experiment')
-first_experiment.run_experiments(result_suffix='_26-9-22_guru_constrained_1_', file_suffix='')
+first_experiment.run_experiments(result_suffix='_26-9-22_freelancer_constrained_1_', file_suffix='')
 print("Finished")
 print("!"*100)
